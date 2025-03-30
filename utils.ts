b389e2546a632a4cfa8dd1c11fa4bef249c02993
Votre Nom <vous@exemple.com>
2025-03-30 17:58:55 +0200

    cleaned

--- utils.ts
+++
@@ -1,6 +0,0 @@
-export const wrap = (value: string, charA: string, charB = charA): string => {
-  if (value[0] === charA && value[value.length - 1] === charB) {
-    return value;
-  }
-  return `${charA}${value}${charB}`;
-}

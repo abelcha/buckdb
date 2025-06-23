ebb3cc800f284e48bed2a07e32651476829fe842
Votre Nom <vous@exemple.com>
2025-06-23 11:44:01 +0200

    netlify config

--- demo/vite.netlify.config.ts
+++ demo/vite.netlify.config.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import * as fs from 'fs'
 import { defineConfig } from 'vite'
+import path from 'node:path'
 const pkg = JSON.parse(
     fs.readFileSync(new URL('./package.json', import.meta.url).pathname).toString(),
 )
@@ -20,7 +21,17 @@ export default defineConfig({
     esbuild: {
         minifySyntax: false,
     },
+    
+    define: {
+        rootDirectory: JSON.stringify(__dirname),
+    },
     resolve: {
         dedupe: ['vscode', ...localDependencies],
+        alias: {
+            // Map an alias to the external directory
+            // '@buckdb': '',
+            '@buckdb/isomorphic': path.resolve(__dirname, '../remote.ts'),
+            '@buckdb': path.resolve(__dirname, '../'),
+        },
     },
 })

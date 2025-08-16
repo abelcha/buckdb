52fdfa230cbc9416353c19ac3d73cb98b5195b37
Votre Nom <vous@exemple.com>
2025-08-16 16:37:52 +0200

    _

--- demo/duckdb-server.sh
+++ demo/duckdb-server.sh
@@ -5,7 +5,7 @@ for pkg in hostfs httpserver
      set --append modules "INSTALL $pkg FROM community; LOAD $pkg; "
 end
 
-set fullcmd "$modules SELECT httpserve_start('0.0.0.0', 9998, '');"
+set fullcmd "$modules SELECT httpserve_start('0.0.0.0', 9999, '');"
 while true 
 	echo $fullcmd
 	echo $fullcmd | DUCKDB_HTTPSERVER_FOREGROUND=1 duckdb
